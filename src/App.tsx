8a6d7f2fad8f4f582be164c8afdfa71911ce47f3
tinph010803 <tinphan924@gmail.com>
2025-09-13 10:32:31 +0700

    add AdminDashboar

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
+  const location = useLocation();
+
+  // Ẩn header khi ở /admin
+  const hideHeader = location.pathname.startsWith("/admin");
   return (
     <>
-      <Header />
+      {!hideHeader && <Header />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
     </>
   );
